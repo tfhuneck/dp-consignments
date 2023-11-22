1b854488f7e0c72befaabac099ff3d569cfb58c0
Timothy Huneck <timothyhuneck@Timothys-MBP.lan>
2023-11-22 14:48:45 -0800

    ebay auth remove

--- client/src/components/hooks/useFetchData.jsx
+++ client/src/components/hooks/useFetchData.jsx
@@ -11,7 +11,7 @@ export const useFetchData = (url) => {
     useEffect(() => {
         async function fetchData(){
             await axios.get(
-                serverUrl +
+                // serverUrl +
                 url,
                 {params:{
                         userAuth
